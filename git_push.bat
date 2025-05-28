ef445d56845ae6fc651277a31ce0280702638f07
cdy <3634043412@qq.com>
2025-05-28 21:22:47 +0800

    picture push

--- git_push.bat
+++ git_push.bat
@@ -1,6 +1,5 @@
 @echo off
-set "current_dir=%~dp0"
-set "git_push_path=%current_dir%git_push_bt.bat"
-start "" "%git_push_path%"
-echo 已执行 git_push.bat
-pause
+git checkout master
+git add .
+git commit -m "picture push"
+git push pictureku master
--- git_push_bt.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git checkout master
-git add .
-git commit -m "picture push"
-git push pictureku master
